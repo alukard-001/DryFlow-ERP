6e6f55df1dc16bb3dcaf4abe690bbac2c7863338
alukard-001 <alukardttn@icloud.com>
2020-10-03 10:41:23 +0000

    Initialize project structure

--- BackEnd/apps/companies/employeers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.companies.employeers'
--- BackEnd/apps/inventory/movements/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'movements'
-
-urlpatterns = [
-    path('', views.MovementListView.as_view(), name='list_movements')
-]
